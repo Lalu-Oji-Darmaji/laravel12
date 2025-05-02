9384765fce6c671d79a718606558fd94fe645257
Lalu-Oji-Darmaji <laluojidarmaji@gmail.com>
2025-05-02 14:55:59 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mahasiswa/{nama}', function ($nama) {
+    return "Nama mahasiswa : " . $nama;
+});
+
+Route::get('/stok_barang/{benda}/{merk}', function ($benda, $merk) {
+    return "Stok barang untuk : " . $benda . " " . $merk;
+});
+
+Route::get('/stok_barang/{benda?}/{merk?}', function ($benda = 'Samsung', $merk = 'Samsung') {
+    return "Stok barang untuk : " . $benda . " " . $merk;
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "Tampilkan User Id : " . $id;
+})->where('id','[0-9]+');
+
+Route::get('/user/{id?}', function ($id) {
+    return "Tampilkan User Id : " . $id;
+})->where('id','^[A-Z{2}0-9]+');
+
+
